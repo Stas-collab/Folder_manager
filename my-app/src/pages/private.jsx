0427a28e6a7cf8edf208707c1d8a18964c5b64ce
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2024-10-22 17:26:50 +0300

    Add structure

---
+++ my-app/src/pages/private.jsx
@@ -0,0 +1,134 @@
+import { signOut } from 'firebase/auth';
+import React, { useState, useEffect } from 'react';
+import { auth, db, storage } from '../firebase';
+import styles from './App.module.css';
+import { Link } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import DefaultImage from '../image/default.jpg';
+
+const Private = () => {
+    const [avatarUrl, setAvatarUrl] = useState(DefaultImage); // Стан для URL аватарки
+
+    useEffect(() => {
+        // Функція для завантаження аватарки
+        const fetchAvatarUrl = async () => {
+            const userDocRef = doc(db, 'users', auth.currentUser.uid); // Отримуємо документ користувача з Firestore
+            const userDoc = await getDoc(userDocRef); // Викликаємо getDoc
+
+            if (userDoc.exists()) {
+                setAvatarUrl(userDoc.data().avatarUrl); // Встановлюємо URL аватарки з Firestore
+            }
+        };
+
+        fetchAvatarUrl(); // Викликаємо функцію
+    }, []);
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => console.log('SignOut'))
+            .catch((error) => console.log(error));
+    };
+    return (
+        <div>
+            <article className={styles.container}>
+                <section className={styles.profil}>
+                    <h1 className={styles.logoName}>Folders manager</h1>
+                    <div className={styles.imgProfil}>
+                        <nav>
+                            <img src={avatarUrl} alt="" className={styles.profilImg} />
+                        </nav>
+                        <p className={styles.userName}>Frog</p>
+                        <div className={`${styles.icon} ${styles.dashboard}`}></div>
+                        <div className={styles.icon}>
+                            <Link to={'/private'} className={styles.links}>
+                                <span className="material-symbols-outlined">home</span>
+                            </Link>
+                            <Link to={'/private'} className={`${styles.links} ${styles.home}`}>
+                                {' '}
+                                Home
+                            </Link>
+                        </div>
+                        <div className={`${styles.icon} ${styles.special}`}>
+                            <div className={styles.icon}>
+                                <button className={`${styles.btn} ${styles.folder}`}>
+                                    <p>Folders</p>
+                                </button>
+                            </div>
+                            <button className={`${styles.btn} ${styles.arrow}`}>
+                                <div className={styles.arrow}></div>
+                            </button>
+                        </div>
+                        <ul className={styles.folders}>
+                            <li>Marketing</li>
+                            <li>Design</li>
+                            <li>WorkPlace</li>
+                        </ul>
+                        <div className={styles.icon}>
+                            <Link to={'/settings'} className={styles.links}>
+                                <span className="material-symbols-outlined">settings</span>
+                            </Link>
+                            <Link to={'/settings'} className={styles.links}>
+                                Settings
+                            </Link>
+                        </div>
+                    </div>
+                </section>
+
+                <section className={styles.userActivites}>
+                    <div className={styles.panel}>
+                        <div className={styles.overview}>
+                            <h1 className={styles.overviewText}>Overview</h1>
+                            <p className={styles.overviewGb}>185 GB</p>
+                        </div>
+                        <div className={styles.rightPanel}>
+                            <div className={styles.userBtn}>
+                                <button className={styles.ring}>
+                                    <span className="material-symbols-outlined">notifications</span>
+                                </button>
+                                <div className={styles.ringWindow} hidden>
+                                    <button className={styles.closeWindow}>&times;</button>
+                                </div>
+                                <button onClick={handleSignOut} className={styles.logout}>
+                                    <span className="material-symbols-outlined">logout</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.manageFolders}>
+                        <h1 className={styles.manageFoldersText}>Manage your folders</h1>
+                        <div className={styles.userFolders}>
+                            <div className={styles.newFolder}>
+                                <button className={styles.crossButton}>&#x2715;</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles.underManage}>
+                        <p>
+                            Create folders to sort files and have
+                            <br /> quick access to documents
+                        </p>
+                    </div>
+                    <div className={styles.createBtnWindow} hidden>
+                        <h1 className={styles.headText}>Створення папки</h1>
+                        <div className={styles.createBtnForm}>
+                            <input
+                                type="text"
+                                name=""
+                                id=""
+                                className={styles.inputNameFolder}
+                                placeholder="Folder name"
+                            />
+                            <p>Виберіть колір</p>
+                            <input type="color" name="" id="" className={styles.inputColorFolder} />
+                            <input type="submit" value="Створити папку" className={styles.inputCreateFolder} />
+                        </div>
+                    </div>
+                </section>
+            </article>
+        </div>
+    );
+};
+
+export default Private;
