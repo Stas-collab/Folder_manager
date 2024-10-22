24ef89becc24a364c37096ab015354b04a184ddc
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2024-10-22 16:46:10 +0300

    Remove maine

--- my-app/cypress/e2e/firstTest.cy.js
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-    it('passes', () => {
-        cy.visit(' http://localhost:9000');
-
-        cy.get('[data-testid="cypress-titel"]').should('exist');
-    });
-});
--- my-app/src/components/protectedRout.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRout = ({ children, user }) => {
-    return user ? children : <Navigate to="/"> </Navigate>;
-};
-
-export default ProtectedRout;
