2972d8e6743000b77257bd39c9a57ba1dfc0ecac
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2024-10-31 15:31:40 +0200

    Add end-2-end tests

--- my-app/cypress/e2e/firstTest.cy.js
+++ my-app/cypress/e2e/firstTest.cy.js
@@ -5,3 +5,33 @@ describe('template spec', () => {
         cy.get('[data-testid="cypress-titel"]').should('exist');
     });
 });
+
+describe('Home Component E2E Tests', () => {
+    beforeEach(() => {
+        cy.visit(' http://localhost:9000');
+    });
+
+    it('should display register form by default', () => {
+        cy.contains('Register');
+        cy.get('input[type="email"]').should('be.visible');
+        cy.get('input[type="password"]').should('be.visible');
+    });
+
+    it('should allow user to register', () => {
+        cy.get('input[type="email"]').type('testuser@ex2amplwd.com');
+        cy.get('input[type="password"]').type('password12');
+        cy.get('button').contains('Create an account').click();
+
+        cy.url().should('include', '/private');
+    });
+
+    it('should display an error for invalid email format', () => {
+        cy.get('input[type="email"]').type('invalid-email');
+        cy.get('input[type="password"]').type('password123');
+        cy.get('button').contains('Create an account').click();
+
+        cy.on('window:alert', (str) => {
+            expect(str).to.equal('Invalid email format');
+        });
+    });
+});
